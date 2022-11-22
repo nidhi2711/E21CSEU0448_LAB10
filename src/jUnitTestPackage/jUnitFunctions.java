4f2b754e55d454001f793d943ee508a51b9505f9
nidhi2711 <e21cseu0448@bennett.edu.in>
2022-11-22 16:48:37 +0530

    This is nidhi's work

--- src/jUnitTestPackage/jUnitFunctions.java
+++ src/jUnitTestPackage/jUnitFunctions.java
@@ -2,4 +2,14 @@ package jUnitTestPackage;
 
 public class jUnitFunctions {
 
-}
+	  public static void main(String[] args) {
+
+	    int dividend = 25, divisor = 4;
+
+	    int quotient = dividend / divisor;
+	    int remainder = dividend % divisor;
+
+	    System.out.println("Quotient = " + quotient);
+	    System.out.println("Remainder = " + remainder);
+	  }
+	}
---
+++ src/jUnitTestPackage/testAddNumber.java
@@ -0,0 +1,14 @@
+package jUnitTestPackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testAddNumber {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
